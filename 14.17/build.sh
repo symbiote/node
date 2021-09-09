68636d6c044611c113b8c6c1bbe5ef698757104f
Amir Sarkis <amir@symbiote.com.au>
2021-09-09 17:10:02 +1000

    Added node versions 14.17 and 16.8

---
+++ 14.17/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker build -t symbiote/node:14.17 .
+
+# and the latest tag also
+docker build -t symbiote/node:14.17 .
---
+++ 16.8/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker build -t symbiote/node:16.8 .
+
+# and the latest tag also
+docker build -t symbiote/node:16.8 .
